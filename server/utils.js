b762999cdcc36091576b68f8d454bfe5f14a76ef
user <user@users-MacBook-Air.local>
2020-02-26 20:54:13 -0700

    weeeeee

--- server/utils.js
+++ server/utils.js
@@ -10,11 +10,13 @@ module.exports = {
       accum[user.id] = user;
       return accum;
     }, {});
-    return posts.map(post => {
-      post.displayName = `${userDict[post.userId].first} ${
-        userDict[post.userId].last
-      }`;
-      return post;
-    });
+    return posts
+      .filter(post => userDict[post.userId])
+      .map(post => {
+        post.displayName = `${userDict[post.userId].first} ${
+          userDict[post.userId].last
+        }`;
+        return post;
+      });
   },
 };
